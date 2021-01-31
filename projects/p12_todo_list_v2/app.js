8eaeffdc9436429fc10e70e13322369adc047f6e
Rob de Boer <roblkdeboer@gmail.com>
2021-01-31 15:13:33 +0800

    Updating twelfth project - style changes and clean up

--- projects/p12_todo_list_v2/app.js
+++ projects/p12_todo_list_v2/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 // const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+require('dotenv').config();
 
 
 const app = express();
@@ -14,7 +15,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+const mongodbpw = process.env.MONGOPW;
+mongoose.connect("mongodb+srv://admin-rob:" + mongodbpw + "@udemy-todolist.qmvor.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 const itemsSchema = {
   name: String
@@ -139,6 +141,11 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
