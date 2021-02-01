c2ec11db1d0c7b0bc9ed5c63f8f273fe336f6f6a
Rob de Boer <roblkdeboer@gmail.com>
2021-02-01 22:44:04 +0800

    Updating thirteenth project - adding cookies, authentication and sessions with passport

--- projects/p13_authentication_test/app.js
+++ projects/p13_authentication_test/app.js
@@ -4,8 +4,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 require('dotenv').config();
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -15,8 +16,24 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+// Setting up the passport session
+app.use(session({
+    secret:"Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+}));
+
+// Initialise and use passport session
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
 // Create the mongoose DB connection on local hose
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
+
 
 // Set up new mongoose schema in the DB
 const userSchema = new mongoose.Schema({
@@ -24,9 +41,19 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
+// Adding the passport as a plugin to hash and salt the passwords
+userSchema.plugin(passportLocalMongoose);
+
 // Set up a new user model
 // Name of the collection = User is created using the userSchema
 const User = new mongoose.model("User", userSchema)
+
+// Configure the local passport to create a local strategy and to serialise and deserialise the code
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 // Render home page
 app.get("/", function(req,res){
@@ -41,21 +68,21 @@ app.get("/login", function(req,res){
 // Take the post request from the form on the login page
 // If the email field matches with the username field
 app.post("/login", function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
 
-    // Find a user based on email, if the email exists, compare the hashed password entered, if it matches, render the secrets page
-    User.findOne({ email: username }, function (err, foundUser) {
+    // New user created from mongoose model taking the data keyed into the login form
+    const user = new User({
+        username: req.body.username,
+        passport: req.body.password
+    });
+
+    // if the login creds are valid, send a cookie to the browser validating the login and allowing to view secret content
+    req.login(user, function(err){
         if (err) {
             console.log(err);
         } else {
-            if (foundUser) {
-                bcrypt.compare(password, foundUser.password, function (err, result) {
-                    if (result === true) {
-                        res.render("secrets");
-                    }
-                });
-            }
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
         }
     });
 });
@@ -65,27 +92,39 @@ app.get("/register", function(req,res){
     res.render("register");
 });
 
+// Render secrets page to allow users to access it if they're still logged in.  If not, they get redirected to the login page
+app.get("/secrets", function(req,res){
+    
+    if (req.isAuthenticated()){
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+})
+
 // Take the post request from the form on the register page
 // Body parser takes the input from name=userName and name=password
 // Use bcrypt to hash and salt the password
-app.post("/register", function(req,res){
-    
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        const newUser = new User({
-            email: req.body.username,
-            password: hash            
-        });
-    
-        // Save the newUser and if there's no error, render the secrets page
-        newUser.save(function(err){
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }
-        });
+app.post("/register", function (req, res) {
+
+    // Set up registering a user with mongoose
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            // If the authentication is successful then trigger the callback
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
+        }
     });
+});
+
+// Render home page after clicking on logout and deauthenticate the user and end the user session
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/");
 });
 
 
